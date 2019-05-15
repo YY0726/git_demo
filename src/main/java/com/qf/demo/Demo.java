d0a8db6cab55c430c8832951965afbbe210d15af
hqq <hqq@qq.com>
2019-05-16 01:02:46 +0800

    Initial commit

---
+++ src/main/java/com/qf/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.qf.demo;
+
+/**
+ * @user 60941
+ * @data 2019/5/16 0:53
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
